0945d47c5a6c8d275e7f76b2a641d2465832cc6c
Evice <kirill.nikula@mail.ru>
2022-01-14 13:54:36 +0300

    1izm

--- todoapp/js/todo-line.js
+++ todoapp/js/todo-line.js
@@ -5,8 +5,6 @@ const todoLine = {
                 {string: 'learn HTML'},
                 {string: 'learn CSS'},
                 {string:'learn JS'},
-                {string:'learn JS'},
-                {string:'learn JS'},
             ]
             ,
             buttonType:[
@@ -19,40 +17,69 @@ const todoLine = {
     },
     template:`
         <div class='todo-search'>
-            <input type='text'>
+            <input type='text' id='todo-search-row' @click='searchItem'>
             <div class="todoList">
-                <div class='line-block All Active' v-for:='(n,index) in 5'>
+                <div class='line-block All Active visibleRow' v-for:='(n,index) in 3'>
                     <input type='checkbox' :id=nameLines[index].string @click='classForm'>
                     <label :for=nameLines[index].string>{{nameLines[index].string}}</label>
                 </div>
             </div>
             <div class="search-type">
                 <div class='button-block' v-for:='(n,index) in 3'>
-                    <input type="radio" name='checklist' class='todo-radio-search-type' :id=buttonType[index].string @click='sortBy'>
+                    <input type="radio" name='checklist' class='todo-radio-search-type' :id=buttonType[index].string @click='sortByRadioId'>
                     <label :for=buttonType[index].string>{{buttonType[index].string}}</label>
                 </div>
             </div>
         </div>
     `,
     methods:{
-        classForm: function(event){ 
+        classForm(event){ 
             event.target.parentNode.classList.toggle('Complited') ;
             event.target.parentNode.classList.toggle('Active') 
         },
-        sortBy: function(event){
+        sortByRadioId(event){
             const all = document.getElementsByClassName('All');
             const id = event.target.id;
             const idList = document.getElementsByClassName(id);
+
+            if(document.querySelector('activeRadioId')){
+                document.querySelector('activeRadioId').classList.remove('activeRadioId');
+            }
+            document.getElementById(id).classList.add('activeRadioId');
+
             for(let i = 0; i< all.length; i++){
-                console.log(all[i]);
                 all[i].classList.remove('visibleRow');
             }
             for(let i = 0; i< idList.length; i++){
-                console.log(idList[i]);
                 idList[i].classList.add('visibleRow');
-            }
-               
+            } 
         },
+        searchItem(event){
+            event.target.addEventListener('keyup',function(){
+                const activeClass = document.getElementsByClassName('visibleRow');
+                const inputValue = document.getElementById('todo-search-row').value;
+
+                /*
+                const id = document.getElementsByClassName('activeRadioId')[0].id;
+                const idList = document.getElementsByClassName(id);
+
+                console.log(idList);
+                for(let i = 0; i< idList.length; i++){
+                    idList[i].classList.add('visibleRow');
+                } 
+*/
+                this.sortByRadioId;
+                for(let i = 0; i < activeClass.length; i++){
+                    if (activeClass[i].querySelector('label').innerHTML.includes(inputValue)){
+                        activeClass[i].classList.add('visibleRow');
+                    }
+                    else{
+                        activeClass[i].classList.remove('visibleRow');
+                    }
+                }
+            });
+            ;
+        }
     }
 }
 
