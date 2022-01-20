7e37f24d44ca90cc875a2ceddee36cbeb5dd4daf
Evice <kirill.nikula@mail.ru>
2022-01-20 18:29:47 +0300

    7

--- findTwo/js/closeImg.js
+++ findTwo/js/closeImg.js
@@ -1,4 +1,5 @@
-const allElem = document.querySelectorAll('.img-cont')
+const allElem = document.querySelectorAll('.img-cont');
+let eArr = [];
 
 allElem.forEach(element=>{
     element.addEventListener('click',()=>{checkTwo(element)})
@@ -9,16 +10,24 @@ function checkTwo(e){
     e.querySelector('div').classList.remove('hideElem');
     e.querySelector('div').classList.add('checkElem');
     let checkElem =  e.parentNode.querySelectorAll(`.checkElem`)
-    if (checkElem.length == 2){
-        checkElem.forEach(elem=>{
-            setTimeout(()=>{
-                elem.classList.remove('checkElem')
-                elem.classList.add('hideElem')
-            },500)
+    eArr.push(e);
+    
+    if (eArr.length == 2 && eArr[0].classList[1] == eArr[1].classList[1]){
+        eArr.forEach(el=>{
+            el.addEventListener('click', (event) => { return false; });
         })
+        eArr = []
+    }else if(eArr.length == 2){
+        returnHide(checkElem);
+        eArr = []
     }
 }
 
-function rightTwo(e){
-    
+function returnHide(a){
+    a.forEach(elem=>{
+        setTimeout(()=>{
+            elem.classList.remove('checkElem')
+            elem.classList.add('hideElem')
+        },500)
+    })
 }
