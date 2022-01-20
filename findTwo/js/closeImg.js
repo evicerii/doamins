114c449daef22e789373b42edee9ba636913b394
Evice <kirill.nikula@mail.ru>
2022-01-20 17:22:46 +0300

    6

--- findTwo/js/closeImg.js
+++ findTwo/js/closeImg.js
@@ -1,18 +1,24 @@
 const allElem = document.querySelectorAll('.img-cont')
 
-
-allElem.forEach(e=>{
-    e.addEventListener('click', ()=>{
-        e.querySelector('div').classList.remove('hideElem');
-        e.querySelector('div').classList.add('checkElem');
-        let checkElem =  e.parentNode.querySelectorAll(`.checkElem`)
-        if (checkElem.length == 2){
-            checkElem.forEach(elem=>{
-                setTimeout(()=>{
-                    elem.classList.remove('checkElem')
-                    elem.classList.add('hideElem')
-                },500)
-            })
-        }
-    })
+allElem.forEach(element=>{
+    element.addEventListener('click',()=>{checkTwo(element)})
 })
+
+//limits the number of checked elems
+function checkTwo(e){
+    e.querySelector('div').classList.remove('hideElem');
+    e.querySelector('div').classList.add('checkElem');
+    let checkElem =  e.parentNode.querySelectorAll(`.checkElem`)
+    if (checkElem.length == 2){
+        checkElem.forEach(elem=>{
+            setTimeout(()=>{
+                elem.classList.remove('checkElem')
+                elem.classList.add('hideElem')
+            },500)
+        })
+    }
+}
+
+function rightTwo(e){
+    
+}
